a47e0c0bd75f0ab6a2aa7d6376243b2cd6f60471
sadyrin <sadyrin@gmail.com>
2023-01-27 18:27:47 +0200

    Done task stage0-module4-conditions-task5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -3,8 +3,8 @@ package school.mjc.stage0.conditions.task5;
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
         if(
-               year % 4 == 0 
-            && year % 100 != 0
+            ((year % 4 == 0) && (year % 100.0 != 0)) 
+            || ((year % 4 == 0) && (year % 100.0 == 0) && (year % 400.0 == 0))
         ) {
             System.out.println("leap");
         }
